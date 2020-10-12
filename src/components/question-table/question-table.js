300de4e56940e735b24137ec66cc0faa9bde00f0
oMid <dehgkaniomid@gmail.com>
2020-10-12 13:17:39 +0330

    queue question for display in question table

--- src/components/question-table/question-table.js
+++ src/components/question-table/question-table.js
@@ -23,10 +23,50 @@ class FunQuestionTable extends React.Component {
         this.onQuestionAnswered = this.onQuestionAnswered.bind(this)
         this.currentOpenQuestion = undefined
         this.currentQuestion = undefined
+        this.openQuestionQueue = []
     }
     onOpenQuestion(obj) {
+        this.openQuestionQueue.push(obj)
+        this.handleNextQuestionQueue()
+    }
+    onCloseQuestion(obj) {
+        console.log('onCloseQuestion', obj)
+        if (this.currentQuestion.questionId != obj.questionId) {
+            throw new Error('tried to close question that is not currently open')
+        }
+
+        this.currentQuestion.answer = obj.answer
+        this.currentQuestion = undefined
+        this.currentOpenQuestion = undefined
+        this.setState({})
+
+        if (this.openQuestionQueue.length != 0) {
+            this.handleNextQuestionQueue()
+        }
+    }
+    onQuestionAnswered(questionId, answer) {
+        this.props.data.onAnswer({
+            questionId: questionId,
+            answer: answer
+        })
+    }
+    handleNextQuestionQueue() {
+
+        if (this.openQuestionQueue.length == 0) {
+            console.log('There is no more question in queue')
+            return
+        }
+
+        if (this.currentQuestion != undefined) {
+            console.log('waiting for current question to finish')
+            return
+        }
+
+        const obj = this.openQuestionQueue.splice(0, 1)[0]
+
         for (let q of this.state.questionTable.rows) {
             if (q.questionId === obj.questionId) {
+                this.currentQuestion = q
 
                 let timerId1 = setInterval(() => {
                     q.showQuestionMark = !q.showQuestionMark
@@ -37,8 +77,8 @@ class FunQuestionTable extends React.Component {
 
                     clearInterval(timerId1)
                     clearTimeout(timerId2)
+
                     this.currentOpenQuestion = obj
-                    this.currentQuestion = q
                     this.setState({})
 
                 }, 1500)
@@ -46,23 +86,6 @@ class FunQuestionTable extends React.Component {
             }
         }
     }
-    onCloseQuestion(obj) {
-        console.log('onCloseQuestion', obj)
-        if (this.currentQuestion.questionId != obj.questionId) {
-            throw new Error('tried to close question that is not currently open')
-        }
-
-        this.currentQuestion.answer = obj.answer
-        this.currentOpenQuestion = undefined
-        this.setState({})
-    }
-    onQuestionAnswered(questionId, answer) {
-        this.props.data.onAnswer({
-            questionId: questionId,
-            answer: answer
-        })
-    }
-
     render() {
 
         let questionElem
--- src/components/question-table/question.js
+++ src/components/question-table/question.js
@@ -9,9 +9,22 @@ export class Question extends React.Component {
         if (props.onAnswer == undefined) {
             throw new Error('onAnswer callback is undefined')
         }
+        this.state = {
+            inputValue: ''
+        }
+        this.updateInputValue = this.updateInputValue.bind(this)
+        this.onButtonClicked = this.onButtonClicked.bind(this)
     }
     onButtonClicked(value) {
         this.props.onAnswer(this.question.questionId, value)
+    }
+    updateInputValue(value) {
+        this.setState({
+            inputValue: value
+        });
+    }
+    onSubmitButtonClicked() {
+        this.props.onAnswer(this.question.questionId, this.state.inputValue)
     }
     render() {
         let elems = []
@@ -28,7 +41,18 @@ export class Question extends React.Component {
                 elems.push(btn)
             }
         } else if (this.openQuestion.answerType === QuestionTableAnswerTypes.Text) {
-            let txtInput = <input type="text" key={`qtxt-${i++}`} />
+            let txtInput =
+
+                <div className="form-row" style={{ width: '100%' }} key="txt-inp">
+                    <div className="form-group col-md-6" style={{ marginTop: '40px' }}>
+                        <label htmlFor="txt-inp">بنویس</label>
+                        <input type="text" value={this.state.inputValue} onChange={event => this.updateInputValue(event.target.value)} id="txt-inp" name="txt-inp" className="form-control" />
+                    </div>
+                    <div className="form-group col-md-2" style={{ marginTop: '40px' }}>
+                        <label htmlFor="btn-submit" style={{ color: 'transparent', userSelect: 'none' }}>I</label>
+                        <button name="btn-submit" onClick={() => this.onSubmitButtonClicked()} className="form-control btn btn-info">اوکی</button>
+                    </div>
+                </div>
             elems.push(txtInput)
         } else {
             throw new Error('invalid answer type: ' + this.openQuestion.answerType)
